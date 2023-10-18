b91b51ccbd1b9135cbf3fb539072d8b0f1b9db6e
stivenVA <stivenv.ariza@gmail.com>
2023-10-18 10:21:12 -0500

    Connection to remote Database

--- src/main/java/com/example/grupob/busquedatesoro/models/User.java
+++ src/main/java/com/example/grupob/busquedatesoro/models/User.java
@@ -1,10 +1,20 @@
 package com.example.grupob.busquedatesoro.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
+@Data
 public class User {
     @Id
+    @Column(name = "id")
     String id;
+
+    String name;
 }
