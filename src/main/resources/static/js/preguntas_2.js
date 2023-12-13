07d961d32194f1b9e0348d2f0a1f5355b62e2172
Anggy21 <anggymichelle21@gmail.com>
2023-12-12 23:34:20 -0500

    Juegos de memorias terminados

--- src/main/resources/static/js/preguntas_2.js
+++ src/main/resources/static/js/preguntas_2.js
@@ -1,123 +1,98 @@
-const questions = [
-    {
-        //1
-        question: '¿Quién es el protagonista principal en la leyenda del Silbón?',
-        image: '../img/silbon.png',
-        answers: [
-            { text: 'Un campesino', correct: false },
-            { text: 'Un cazador', correct: false },
-            { text: 'Un fantasma errante', correct: true },
-            { text: 'Un jinete misterioso', correct: false }
-        ]
-    },
-    {
-        question: '¿Cuál es el principal atractivo del Mohán según la leyenda?',
-        image: '../img/mohan.png',
-        answers: [
-            { text: 'Su canto melodioso', correct: false },
-            { text: 'Su habilidad para transformarse', correct: false },
-            { text: 'Su gran riqueza', correct: false },
-            { text: 'Sus poderes mágicos', correct: true }
-        ]
-    },
-    {
-        question: '¿Cuál es el propósito del Chullachaqui al engañar a los viajeros?',
-        image: '../img/mitoChullachaqui.png',
-        answers: [
-            { text: 'Proteger la selva', correct: false },
-            { text: 'Castigar a los curiosos', correct: true },
-            { text: 'Robar sus pertenencias', correct: false },
-            { text: 'Jugar bromas inofensivas', correct: false }
-        ]
-    },
-];
+let cardsOpen = 0;
+let card1 = null;
+let card2 = null;
+let firstResult = null;
+let secondResult = null;
+let movements = 0;
+let successes = 0;
+let timer = false;
+let time = 60;
+let startTime = time;
+let regressiveTimeId = null;
 
-let currentQuestionIndex = 0;
-let correctAnswers = 0;
-let allQuestionsCorrect = false;
+let showMovements = document.getElementById('movements');
+let showSuccesses = document.getElementById('successes');
+let showTime = document.getElementById('time');
 
-const questionText = document.getElementById('question-text');
-const answerButtons = document.getElementById('answer-buttons');
-const nextButton = document.getElementById('next-button');
+let winAudio = new Audio('../sounds/win.wav');
+let loseAudio = new Audio('../sounds/lose.wav');
+let clickAudio = new Audio('../sounds/click.wav');
+let rightAudio = new Audio('../sounds/right.wav');
+let wrongAudio = new Audio('../sounds/wrong.wav');
 
-function startGame() {
-    currentQuestionIndex = 0;
-    correctAnswers = 0;
-    allQuestionsCorrect = false;
-    showQuestion(questions[currentQuestionIndex]);
-}
-function goBack() {
-    // Cambia la URL según tu estructura de carpetas y archivos
-    window.location.href = '../html/mapa.html';
-}
-function showQuestion(question) {
-    questionText.innerText = question.question;
-    const imageElement = document.getElementById('question-image');
-    imageElement.src = question.image;
-    imageElement.alt = `Imagen de la pregunta: ${question.question}`;
+let numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+numbers = numbers.sort(()=>{return Math.random() -0.5});
+console.log(numbers);
 
-    answerButtons.innerHTML = '';
-    question.answers.forEach(answer => {
-        const button = document.createElement('button');
-        button.innerText = answer.text;
-        button.classList.add('btn');
-        button.addEventListener('click', () => selectAnswer(answer, button));
-        answerButtons.appendChild(button);
-    });
-}
+function countTime(){
+    regressiveTimeId = setInterval(()=>{
+        time--;
+        showTime.innerHTML = `Tiempo: ${time} segundos`;
 
-function selectAnswer(answer, button) {
-    const correct = answer.correct;
-    if (correct) {
-        // Incrementa el contador de respuestas correctas
-        correctAnswers++;
-    }
-
-    const allButtons = document.querySelectorAll('.btn');
-    allButtons.forEach(btn => btn.classList.remove('selected'));
-    button.classList.add('selected');
-    nextButton.style.display = 'block';
-}
-
-function nextQuestion() {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-        showQuestion(questions[currentQuestionIndex]);
-        nextButton.style.display = 'block';
-    } else {
-        checkAllQuestionsCorrect()
-    }
-}
-function checkAllQuestionsCorrect() {
-    if (correctAnswers === questions.length) {
-        allQuestionsCorrect = true;
-    }
-    showResultMessage();
-}
-function showResultMessage() {
-    nextButton.style.display = 'none';
-    let resultMessage;
-    if (allQuestionsCorrect) {
-        resultMessage = `¡Aprobado! Puntaje final: ${correctAnswers}/${questions.length}`;
-    } else {
-        resultMessage = `Reprobado. Puntaje final: ${correctAnswers}/${questions.length}`;
-    }
-    Swal.fire({
-        title: 'Resultado',
-        text: resultMessage,
-        icon: (correctAnswers === questions.length) ? 'success' : 'error',
-        confirmButtonText: 'Cerrar',
-        showCancelButton: true,
-        cancelButtonText: 'Reintentar'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            // El botón "Cerrar" fue clicado
-            window.location.href = '../html/mapa.html';
-        } else if (result.dismiss === Swal.DismissReason.cancel) {
-            // El botón "Reintentar" fue clicado
-            startGame();
+        if (time == 0){
+            clearInterval(regressiveTimeId);
+            blockCards();
+            loseAudio.play();
+            showTime.innerHTML = `¡Perdiste! Se ha terminado el tiempo`;
         }
-    });
+    }, 1000)
 }
-// Inicia el juego al cargar la página
-startGame();
+
+function blockCards(){
+    for (let i=0; i<=15; i++){
+        let blockCard = document.getElementById(i);
+        blockCard.innerHTML = `<img src="../img/${numbers[i]}.png">`;
+        blockCard.disabled = true;
+    }
+}
+function uncover(id){
+    if (timer == false){
+        countTime();
+        timer = true;
+    }
+
+    cardsOpen++;
+
+    if (cardsOpen == 1){
+        card1 = document.getElementById(id);
+        firstResult = numbers[id];
+        card1.innerHTML = `<img src="../img/${firstResult}.png">`;
+        clickAudio.play();
+
+        card1.disabled = true;
+    }else if (cardsOpen == 2){
+        card2 = document.getElementById(id);
+        secondResult = numbers[id];
+        card2.innerHTML = `<img src="../img/${secondResult}.png">`;
+
+        card2.disabled = true;
+        movements++;
+
+        showMovements.innerHTML = `Movimientos: ${movements}`;
+
+        if (firstResult == secondResult){
+            cardsOpen = 0;
+            successes++;
+            showSuccesses.innerHTML = `Aciertos: ${successes}`;
+            rightAudio.play();
+
+            if (successes == 8){
+                winAudio.play();
+                clearInterval(regressiveTimeId);
+                showSuccesses.innerHTML  = `Aciertos: ${successes} :o!!`;
+                showTime.innerHTML = `Fantástico!! solo demoraste ${startTime - time} segundos`;
+                showMovements.innerHTML = `Movimientos: ${movements} :)`;
+            }
+        }else{
+            wrongAudio.play();
+            setTimeout(()=>{
+                card1.innerHTML = ' ';
+                card2.innerHTML = ' ';
+                card1.disabled = false;
+                card2.disabled = false;
+                cardsOpen = 0;
+
+            },800)
+        }
+    }
+}
---
+++ src/main/resources/static/js/preguntas_8.js
@@ -0,0 +1,98 @@
+let cardsOpen = 0;
+let card1 = null;
+let card2 = null;
+let firstResult = null;
+let secondResult = null;
+let movements = 0;
+let successes = 0;
+let timer = false;
+let time = 30;
+let startTime = time;
+let regressiveTimeId = null;
+
+let showMovements = document.getElementById('movements');
+let showSuccesses = document.getElementById('successes');
+let showTime = document.getElementById('time');
+
+let winAudio = new Audio('../sounds/win.wav');
+let loseAudio = new Audio('../sounds/lose.wav');
+let clickAudio = new Audio('../sounds/click.wav');
+let rightAudio = new Audio('../sounds/right.wav');
+let wrongAudio = new Audio('../sounds/wrong.wav');
+
+let numbers = [17,17,18,18,19,19,20,20,21,21,22,22,23,23,24,24];
+numbers = numbers.sort(()=>{return Math.random() -0.5});
+console.log(numbers);
+
+function countTime(){
+    regressiveTimeId = setInterval(()=>{
+        time--;
+        showTime.innerHTML = `Tiempo: ${time} segundos`;
+
+        if (time == 0){
+            clearInterval(regressiveTimeId);
+            blockCards();
+            loseAudio.play();
+            showTime.innerHTML = `¡Perdiste! Se ha terminado el tiempo`;
+        }
+    }, 1000)
+}
+
+function blockCards(){
+    for (let i=0; i<=15; i++){
+        let blockCard = document.getElementById(i);
+        blockCard.innerHTML = `<img src="../img/${numbers[i]}.png">`;
+        blockCard.disabled = true;
+    }
+}
+function uncover(id){
+    if (timer == false){
+        countTime();
+        timer = true;
+    }
+
+    cardsOpen++;
+
+    if (cardsOpen == 1){
+        card1 = document.getElementById(id);
+        firstResult = numbers[id];
+        card1.innerHTML = `<img src="../img/${firstResult}.png">`;
+        clickAudio.play();
+
+        card1.disabled = true;
+    }else if (cardsOpen == 2){
+        card2 = document.getElementById(id);
+        secondResult = numbers[id];
+        card2.innerHTML = `<img src="../img/${secondResult}.png">`;
+
+        card2.disabled = true;
+        movements++;
+
+        showMovements.innerHTML = `Movimientos: ${movements}`;
+
+        if (firstResult == secondResult){
+            cardsOpen = 0;
+            successes++;
+            showSuccesses.innerHTML = `Aciertos: ${successes}`;
+            rightAudio.play();
+
+            if (successes == 8){
+                winAudio.play();
+                clearInterval(regressiveTimeId);
+                showSuccesses.innerHTML  = `Aciertos: ${successes} :o!!`;
+                showTime.innerHTML = `Fantástico!! solo demoraste ${startTime - time} segundos`;
+                showMovements.innerHTML = `Movimientos: ${movements} :)`;
+            }
+        }else{
+            wrongAudio.play();
+            setTimeout(()=>{
+                card1.innerHTML = ' ';
+                card2.innerHTML = ' ';
+                card1.disabled = false;
+                card2.disabled = false;
+                cardsOpen = 0;
+
+            },800)
+        }
+    }
+}
